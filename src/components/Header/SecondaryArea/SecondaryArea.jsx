51e7edc025b0b6878925d4c27e130b728f4c1227
Artem Lukianets <mail.lukianets@gmail.com>
2020-07-26 13:47:33 +0300

    Some styles edits

--- src/components/Header/SecondaryArea/SecondaryArea.jsx
+++ src/components/Header/SecondaryArea/SecondaryArea.jsx
@@ -1,43 +1,52 @@
 import {NavLink} from "react-router-dom"
 import React from "react"
 import '../../../scss/header/header.scss'
-import LocationOnIcon from '@material-ui/icons/LocationOn'
-import HelpOutlineIcon from '@material-ui/icons/HelpOutline'
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined'
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
-import PersonIcon from '@material-ui/icons/Person'
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline'
 
 export const SecondaryArea = () => {
     return (
         <div className={'secondary-area'}>
-            <div className={'secondary-area-content'}>
+            <div className={'secondary-area-content'} id={'account-link'}>
                 <NavLink to={`/showrooms`} className={'secondary-area-item'}>
-                    <LocationOnIcon className={'close-icon'}/>
+                    <LocationOnOutlinedIcon className={'secondary-area-icon'}/>
                     <p className={'secondary-area-title'}>showroom</p>
                 </NavLink>
             </div>
             <div className={'secondary-area-content'}>
                 <NavLink to={`/help`} className={'secondary-area-item'}>
-                    <HelpOutlineIcon className={'close-icon'}/>
+                    <HelpOutlineIcon className={'secondary-area-icon'}/>
                     <p className={'secondary-area-title'}>faq</p>
                 </NavLink>
             </div>
-            <div className={'secondary-area-content'}>
+            <div className={'secondary-area-content'} id={'account-link'}>
                 <NavLink to={`/customer/account`} className={'secondary-area-item'}>
-                    <PersonIcon className={'close-icon'}/>
+                    <PersonOutlineOutlinedIcon className={'secondary-area-icon'}/>
                     <p className={'secondary-area-title'}>account</p>
+                </NavLink>
+                <div className={'secondary-area-dropdown'}>
+                    <div className={'secondary-area-dropdown-content'}>
+                        <NavLink to={'/customer/account'}>My Account</NavLink>
+                        <NavLink to={'/customer/find-your-style'}>My Style</NavLink>
+                        <NavLink to={'/cart'}>My Cart</NavLink>
+                        <NavLink to={'/customer/details'}>My Details</NavLink>
+                        <NavLink to={'/customer/address'}>My Address Book</NavLink>
+                    </div>
+                </div>
+            </div>
+            <div className={'secondary-area-content'}>
+                <NavLink to={`/customer/cart`} className={'secondary-area-item'}>
+                    <ShoppingCartOutlinedIcon className={'secondary-area-icon'}/>
+                    <p className={'secondary-area-title'}>cart</p>
                 </NavLink>
             </div>
             <div className={'secondary-area-content'}>
                 <NavLink to={`/customer/wishlist`} className={'secondary-area-item'}>
-                    <FavoriteBorderIcon className={'close-icon'}/>
+                    <FavoriteBorderIcon className={'secondary-area-icon'}/>
                     <p className={'secondary-area-title'}>wishlist</p>
-                </NavLink>
-            </div>
-            <div className={'secondary-area-content'}>
-                <NavLink to={`/customer/cart`} className={'secondary-area-item'}>
-                    <ShoppingBasketIcon className={'close-icon'}/>
-                    <p className={'secondary-area-title'}>basket</p>
                 </NavLink>
             </div>
         </div>
